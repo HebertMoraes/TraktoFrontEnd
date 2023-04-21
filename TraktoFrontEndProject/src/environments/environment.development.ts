7bb0abe81a104580005df9e856c9258cd7dbaf30
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2023-04-21 17:52:16 -0300

    correção nome da variavel environment
    .

--- TraktoFrontEndProject/src/environments/environment.development.ts
+++ TraktoFrontEndProject/src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
     urlSignInEmailPassword:'https://api.trakto.io/auth/signin',
+    urlGetAllDesign:'https://api.trakto.io/document?total_per_page=10&order_by=title&order_orientation=desc',
+    urlGetSingleDesign: 'https://api.trakto.io/document/',
     access_token:'access_token'
 };
