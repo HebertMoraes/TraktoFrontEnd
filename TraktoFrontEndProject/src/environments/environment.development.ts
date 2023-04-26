6fdcadae7deeefcdf7c8c03d0d7f93af69447c07
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2023-04-26 20:06:59 -0300

    mudado o link de editar para apresentação
    .

--- TraktoFrontEndProject/src/environments/environment.development.ts
+++ TraktoFrontEndProject/src/environments/environment.development.ts
@@ -3,6 +3,6 @@ export const environment = {
     urlSignInEmailPassword:'https://api.trakto.io/auth/signin',
     urlGetAllDesign:'https://api.trakto.io/document',
     urlGetSingleDesign: 'https://api.trakto.io/document/',
-    urlEditDesign: 'https://editor.trakto.io/editor/',
+    urlEditDesign: 'https://editor.trakto.io/presentation/p/',
     access_token:'access_token'
 };
